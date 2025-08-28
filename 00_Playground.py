fb9c99fd3bc716bc0e6148db359380011e1f7454
albrp97 <107321140+albrp97@users.noreply.github.com>
2025-08-28 15:31:01 +0200

    refined model testing

---
+++ 00_Playground.py
@@ -0,0 +1,8 @@
+# list directories
+import os
+
+print("Current working directory:", os.getcwd())
+print("List of directories:")
+for item in os.listdir():
+    if os.path.isdir(item):
+        print(" -", item)
