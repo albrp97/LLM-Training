87364dd7431939bb8bf6922c0ee3e1f186cf04af
pablolobat0 <plobatorey@gmail.com>
2025-09-08 12:08:37 +0200

    feat: enhance PEFT configuration with VeRa and DoRa support

--- Fine-tuning/1_Train.py
+++ Fine-tuning/1_Train.py
@@ -4,18 +4,17 @@ import torch
 import pandas as pd
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from trl import SFTTrainer, SFTConfig
-from peft import prepare_model_for_kbit_training, LoraConfig, get_peft_model
+from peft import prepare_model_for_kbit_training, LoraConfig, get_peft_model, VeraConfig
 from datasets import Dataset
-
 
 
 # -----------------------------------------------------------
 # User configuration
 # -----------------------------------------------------------
 
-train=True
+train = True
 
-DATASET_CHOICE = "openmath"       # options: "arc", "squad", "openmath"   
+DATASET_CHOICE = "openmath"       # options: "arc", "squad", "openmath"
 # we will not test boolq for now
 
 FINETUNING = "SFT"
@@ -24,13 +23,15 @@ MODEL_NAME = "Qwen/Qwen3-1.7B"
 
 device_map = {"": 0} if torch.cuda.is_available() else {"": "cpu"}
 
+
+PEFT_CONFIG = "LoRa"  # options: "NoPeft", "LoRa", "VeRa", "DoRa"
 # -----------------------------------------------------------
-# LoRA hyperparameters
+# LoRa hyperparameters
 # -----------------------------------------------------------
 
 lora_r = 1024
 # 32 is 1.5% --
-# 64 is 3% 
+# 64 is 3%
 # 128 is 5.8%
 # 256 is 11% --
 # 512 is 20%
@@ -40,7 +41,16 @@ target_modules = ["q_proj", "k_proj", "v_proj", "o_proj"]
 lora_alpha = 16
 lora_dropout = 0.1
 
-LORA_CONFIG = f"LoRa_ {lora_r}" # options: "NoLoRa", "LoRa_{lora_r}"
+# -----------------------------------------------------------
+# VeRa hyperparameters
+# -----------------------------------------------------------
+
+# VeRA parameter dimension (“rank”). Choose higher values than LoRA ranks here, since VeRA uses far fewer parameters than LoRA 
+vera_r = 256
+target_modules = ["q_proj", "k_proj", "v_proj", "o_proj"]
+vera_dropout = 0.1
+# Initial init value for vera_lambda_d vector used when initializing the VeRA parameters. Small values (<=0.1) are recommended
+vera_d_initial = 0.1
 
 QUANT_METHOD = "NoQuant"  # options: "NoQuant", "QLORA", "AWQ", "GPTQ"
 
@@ -49,23 +59,23 @@ QUANT_METHOD = "NoQuant"  # options: "NoQuant", "QLORA", "AWQ", "GPTQ"
 # -----------------------------------------------------------
 
 # Batch size per GPU during training (effective batch size grows with gradient accumulation)
-per_device_train_batch_size = 1  
+per_device_train_batch_size = 1
 
 # Batch size per GPU during evaluation
-per_device_eval_batch_size = 8  
+per_device_eval_batch_size = 8
 
 # Number of steps to accumulate gradients before performing a backward/update pass.
 # Useful when GPU memory is limited and batch size is very small.
-gradient_accumulation_steps = 8  
+gradient_accumulation_steps = 8
 
 # Learning rate for optimizer. Common values for fine-tuning range between 1e-5 and 1e-4.
-learning_rate = 1e-5  
+learning_rate = 1e-5
 
 # Weight decay for regularization (helps prevent overfitting)
-weight_decay = 0.0  
+weight_decay = 0.0
 
 # Maximum allowed gradient norm (gradient clipping). Prevents exploding gradients.
-max_grad_norm = 1.0  
+max_grad_norm = 1.0
 
 # Number of full passes (epochs) over the training dataset
 num_train_epochs = 1
@@ -74,26 +84,26 @@ num_train_epochs = 1
 max_steps = -1
 
 # Warmup ratio for the learning rate schedule (fraction of training used to "warm up")
-warmup_ratio = 0.06  
+warmup_ratio = 0.06
 
 # Frequency (in steps) at which training logs are reported
-logging_steps = 10  
+logging_steps = 10
 
 # If True, groups training samples by similar length for efficiency
-group_by_length = False  
+group_by_length = False
 
 # Enables gradient checkpointing to reduce memory usage.
 # Slows training slightly but allows fine-tuning larger models.
-gradient_checkpointing = True  
+gradient_checkpointing = True
 
 # Concatenates multiple training examples into a single sequence to improve efficiency
-packing = False  
+packing = False
 
 # If True, loss is only computed on assistant responses (ignores user/system messages)
-assistant_only_loss = False  
+assistant_only_loss = False
 
 # Maximum sequence length (in tokens) for tokenized inputs
-max_length = 1024  
+max_length = 1024
 
 
 # -----------------------------------------------------------
@@ -110,7 +120,6 @@ def load_model_and_tokenizer(model_name, quantization_config, device_map):
         torch_dtype=torch.bfloat16 if torch.cuda.is_available() else None,
     )
 
-    
     if not tokenizer.chat_template:
         tokenizer.chat_template = """{% for message in messages %}
                 {% if message['role'] == 'system' %}System: {{ message['content'] }}\n
@@ -118,12 +127,13 @@ def load_model_and_tokenizer(model_name, quantization_config, device_map):
                 {% elif message['role'] == 'assistant' %}Assistant: {{ message['content'] }} <|endoftext|>
                 {% endif %}
                 {% endfor %}"""
-    
+
     # Tokenizer config
     if not tokenizer.pad_token:
         tokenizer.pad_token = tokenizer.eos_token
-        
+
     return model, tokenizer
+
 
 def preprocess_function(df, context: bool):
     if context:
@@ -139,6 +149,7 @@ def preprocess_function(df, context: bool):
 
     # Convert the resulta to HuggingFace Dataset
     return Dataset.from_list(processed_data.tolist())
+
 
 def safe_serialize(obj):
     """Converts no serializable objects to serializable formats."""
@@ -156,7 +167,8 @@ def safe_serialize(obj):
         return {k: safe_serialize(v) for k, v in vars(obj).items() if not k.startswith('_')}
     else:
         return str(obj)
-    
+
+
 def drop_nulls(obj):
     """Eliminate keys with None values recursively."""
     if isinstance(obj, dict):
@@ -169,6 +181,7 @@ def drop_nulls(obj):
 # ============================================================
 # Dataset selection
 # ============================================================
+
 
 if DATASET_CHOICE == "arc":
     df = pd.read_parquet("Datasets/train-ai2_arc.parquet")
@@ -191,11 +204,10 @@ print(f"\nLoaded dataset: {DATASET_CHOICE}\nNumber of samples: {len(df)}\n")
 
 match QUANT_METHOD:
     case "QLORA":
-        load_in_4bit = True # Load the model in 4-bit
+        load_in_4bit = True  # Load the model in 4-bit
         bnb_4bit_quant_type = "nf4"
-        bnb_4bit_use_double_quant = True # Saves more memory at no additional performance
-        bnb_4bit_compute_dtype = torch.bfloat16 
-
+        bnb_4bit_use_double_quant = True  # Saves more memory at no additional performance
+        bnb_4bit_compute_dtype = torch.bfloat16
 
         quantization_config = BitsAndBytesConfig(
             load_in_4bit=load_in_4bit,
@@ -221,24 +233,49 @@ match QUANT_METHOD:
 
     case _:
         quantization_config = None
-        
+
 model, tokenizer = load_model_and_tokenizer(MODEL_NAME, quantization_config, device_map)
 model = prepare_model_for_kbit_training(model)
 
-config = LoraConfig(
-    r=lora_r, 
-    lora_alpha=lora_alpha, 
-    target_modules=target_modules, 
-    lora_dropout=lora_dropout, 
-    bias="none", 
-    task_type="CAUSAL_LM"
-)
+match PEFT_CONFIG:
+    case "LoRa":
+        PEFT_CONFIG += f"{lora_r}"
+        peft_config = LoraConfig(
+            r=lora_r,
+            lora_alpha=lora_alpha,
+            target_modules=target_modules,
+            lora_dropout=lora_dropout,
+            bias="none",
+            task_type="CAUSAL_LM"
+        )
+    case "DoRa":
+        PEFT_CONFIG += f"{lora_r}"
+        peft_config = LoraConfig(
+            r=lora_r,
+            lora_alpha=lora_alpha,
+            target_modules=target_modules,
+            lora_dropout=lora_dropout,
+            bias="none",
+            task_type="CAUSAL_LM",
+            use_dora=True
+        )
+    case "VeRa":
+        peft_config = VeraConfig(
+            r=vera_r,
+            target_modules=target_modules,
+            dropout=vera_dropout,
+            d_initial=vera_d_initial,
+            bias="none",
+            task_type="CAUSAL_LM"
+        )
+    case _:
+        peft_config = None
 
-model = get_peft_model(model, config)
+model = get_peft_model(model, peft_config)
 
 # Output folder where both checkpoints and the final fine-tuned model will be saved
 model_name = MODEL_NAME.split("/")[-1]
-new_model_name = f"{model_name}-{DATASET_CHOICE}_{FINETUNING}_{LORA_CONFIG}_{QUANT_METHOD}" 
+new_model_name = f"{model_name}-{DATASET_CHOICE}_{FINETUNING}_{PEFT_CONFIG}_{QUANT_METHOD}"
 output_dir = f"Models/{new_model_name}" 
 
 # Preprocess the dataset into tokenized format
@@ -268,7 +305,7 @@ sft_config = SFTConfig(
 sft_trainer = SFTTrainer(
     model=model,
     args=sft_config,
-    train_dataset=train_dataset, 
+    train_dataset=train_dataset,
     processing_class=tokenizer,
 )
 
@@ -332,8 +369,6 @@ if train:
     model.save_pretrained(output_dir, safe_serialization=True)
     tokenizer.save_pretrained(output_dir)
 
-
-
     # Create complete metadata report
     training_metadata = {
         "model_info": {
@@ -347,7 +382,7 @@ if train:
             "percentage_trainable": percentage_trainable
         },
         "training_parameters": safe_serialize(sft_trainer.args),
-        "lora_config": safe_serialize(config),
+        "peft_config": safe_serialize(peft_config),
         "quantization_config": safe_serialize(quantization_config) if quantization_config else None,
         "training_stats": {
             "total_steps": sft_trainer.state.max_steps,
@@ -370,7 +405,7 @@ if train:
 
     print(f"\nModel saved in: {output_dir}")
     print("File structure:")
-    print(f"  - model.safetensors")
-    print(f"  - config.json")
-    print(f"  - training_metadata.json")
-    print(f"  - tokenizer files")
+    print("  - model.safetensors")
+    print("  - config.json")
+    print("  - training_metadata.json")
+    print("  - tokenizer files")
